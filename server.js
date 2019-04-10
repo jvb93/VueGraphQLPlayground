c43e119bc5f398bd94ade4adfa1ea7240bacdc30
JVB93 <1976577+jvb93@users.noreply.github.com>
2019-04-09 19:41:52 -0700

    Basic user/post schema with add/query functionality in mongo

--- server.js
+++ server.js
@@ -1,47 +1,30 @@
 const { ApolloServer, gql } = require('apollo-server')
+const mongoose = require('mongoose')
+const fs = require('fs')
+const path = require('path')
 
-const todos = [
-    { task: 'Wash car', completed: false},
-    { task: 'Clean room', completed: true},
-]
+const filepath = path.join(__dirname, 'typeDefs.gql')
+const typeDefs = fs.readFileSync(filepath, 'utf-8')
+const resolvers = require('./resolvers')
 
-const typeDefs = gql`
-    type Todo {
-        task: String,
-        completed: Boolean
-    }
+require('dotenv').config({path: 'variables.env'})
 
-    type Query {
-        getTodos: [Todo]
-    }
+const User = require('./models/User')
+const Post = require('./models/Post')
 
-    type Mutation {
-        addTodo(task: String, completed: Boolean): Todo
-    }
-`
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('DB Connected'))
+.catch(err => console.error(err))
 
-const resolvers = {
-    Query: {
-        getTodos: () => todos
-    },
-
-    Mutation:{
-        addTodo: (_, {task, completed}) => {
-            
-            const todo = {
-                task,
-                completed
-            }
-
-            todos.push(todo)
-            return todo
-        }
-    }
-}
 
 const server = new ApolloServer({
-    typeDefs, resolvers
-})
+    typeDefs,
+    resolvers,
+    context: {
+        User,
+        Post
+    }
+}) 
 
 server.listen().then(({ url }) => {
     console.log(`server listening on ${ url }`)
