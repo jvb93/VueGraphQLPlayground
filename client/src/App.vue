dd3b2ee59a5b32436791c41b3c21698c2caa09ad
JVB93 <1976577+jvb93@users.noreply.github.com>
2019-04-10 17:25:09 -0700

    vuetify installed

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,92 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app style="background: #E3E3EE">
+
+    <!-- Side Navbar -->
+    <v-navigation-drawer app temporary fixed v-model="sideNav">
+      <v-toolbar color="accent" dark flat>
+        <v-toolbar-side-icon @click="toggleSideNav()"></v-toolbar-side-icon>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          <h1 class="title pl-3">VueShare</h1>
+        </router-link>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+       <!-- Side Navbar Links -->
+       <v-list>
+         <v-list-tile v-for="item in sideNavItems" :key="item.title" :to="item.link" ripple>
+           <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+           </v-list-tile-action>
+           <v-list-tile-content>
+             {{item.title}}
+           </v-list-tile-content>
+         </v-list-tile>
+       </v-list>
+    </v-navigation-drawer>
+
+    <!-- Horizontal Navbar -->
+    <v-toolbar fixed color="primary" dark>
+      <!-- App Title -->
+      <v-toolbar-side-icon @click="toggleSideNav()"></v-toolbar-side-icon>
+      <v-toolbar-title class="hidden-xs-only">
+        <router-link to="/" tag="span" style="cursor: pointer">
+          VueShare
+        </router-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- Search Input -->
+      <v-text-field flex prepend-icon="search" placeholder="Search posts" color="accent" single-line hide-details></v-text-field>
+
+      <v-spacer></v-spacer>
+
+      <!-- Horizontal Navbar Links -->
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="item in horizontalNavItems" :key="item.title" :to="item.link">
+          <v-icon class="hidden-sm-only" left>{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <!-- App Content -->
+    <main>
+      <v-container class="mt-4">
+        <router-view/>
+      </v-container>
+    </main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      sideNav: false
+    }
+  },
+  methods:{
+    toggleSideNav() {
+      this.sideNav = !this.sideNav
+    }
+  },
+  computed: {
+    horizontalNavItems() {
+      return [
+        { icon: "chat", title: "Posts", link: "/posts" },
+        { icon: "lock_open", title: "Sign In", link: "/signin" },
+        { icon: "create", title: "Sign Up", link: "/signup" }
+      ];
+    },
+    sideNavItems() {
+      return [
+        { icon: "chat", title: "Posts", link: "/posts" },
+        { icon: "lock_open", title: "Sign In", link: "/signin" },
+        { icon: "create", title: "Sign Up", link: "/signup" }
+      ];
+    }
+  }
+};
+</script>
