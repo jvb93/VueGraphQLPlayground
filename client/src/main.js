f8c237d557805b6fdf4d5ca29814d93361a3404a
JVB93 <1976577+jvb93@users.noreply.github.com>
2019-04-10 18:11:28 -0700

    getting posts via gql and apollo, image carousel

--- client/src/main.js
+++ client/src/main.js
@@ -5,9 +5,21 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
+
+Vue.use(VueApollo)
+
+const defaultClient = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
+const apolloProvider = new VueApollo({defaultClient})
+
 Vue.config.productionTip = false
 
 new Vue({
+  apolloProvider,
   store,
   router,
   render: h => h(App)
